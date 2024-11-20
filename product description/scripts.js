e96f251692437f998af6a0df3a07e0956cba2cb6
Broccoli <22130191@st.hcmuaf.edu.vn>
2024-11-21 00:27:39 +0700

    edit page chi tiet san pham

--- product description/scripts.js
+++ product description/scripts.js
@@ -1,40 +1,56 @@
-// Ẩn thành phần sản phẩm
-$(document).ready(function(){
-    $("#toggleIcon").click(function(){
-        // Kiểm tra xem phần tử #ingredientsContent có đang ẩn không
-        if ($("#ingredientsContent").hasClass("d-none")) {
-            // Hiện phần thành phần và đổi icon thành dấu trừ
-            $("#ingredientsContent").removeClass("d-none");
+$(document).ready(function() {
+    // Toggle ingredients visibility with icon change
+    $("#toggleIcon").click(function() {
+        const ingredientsContent = $("#ingredientsContent");
+        const isHidden = ingredientsContent.hasClass("d-none");
+
+        // Toggle visibility
+        ingredientsContent.toggleClass("d-none", !isHidden);
+
+        // Change the icon
+        if (isHidden) {
             $(this).removeClass("bi-plus").addClass("bi-dash");
         } else {
-            // Ẩn phần thành phần và đổi icon thành dấu cộng
-            $("#ingredientsContent").addClass("d-none");
             $(this).removeClass("bi-dash").addClass("bi-plus");
         }
     });
-});
-let quantity = 1;
-document.getElementById('decrease').onclick = () => {
-    if (quantity > 1) {
-        quantity--;
-        document.getElementById('quantity').textContent = quantity;
-    }
-};
-document.getElementById('increase').onclick = () => {
-    quantity++;
-    document.getElementById('quantity').textContent = quantity;
-};
-// đổi ảnh khi nhấp 
-const thumbnails = document.querySelectorAll('.img-60px');
 
-// Get the main image element where the larger image will be displayed
-const mainImage = document.getElementById('mainImage');
+    // Update price based on quantity
+    const updatePrice = (quantity) => {
+        const basePrice = 4200000;  // Set base price for your product (you can get it from HTML)
+        const totalPrice = basePrice * quantity;
+        $("#prices").text(totalPrice.toLocaleString('vi-VN') + '₫');
+    };
 
-// Attach click event to each thumbnail
-thumbnails.forEach((thumbnail) => {
-    thumbnail.addEventListener('click', (e) => {
-        // Change the source of the main image to the clicked thumbnail's data-large-image value
-        mainImage.src = e.target.getAttribute('data-large-image');
+    // Handle quantity changes
+    let quantity = 1;  // Default quantity
+
+    // When decrease button is clicked
+    $("#decrease").click(function() {
+        if (quantity > 1) {
+            quantity--;  // Decrease the quantity
+            $("#quantity").text(quantity);  // Update the quantity display
+            updatePrice(quantity);  // Update price
+        }
     });
-});
 
+    // When increase button is clicked
+    $("#increase").click(function() {
+        quantity++;  // Increase the quantity
+        $("#quantity").text(quantity);  // Update the quantity display
+        updatePrice(quantity);  // Update price
+    });
+
+    // Change main image when thumbnail is clicked
+    const thumbnails = document.querySelectorAll('.img-60px');
+    const mainImage = document.getElementById('mainImage');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', (e) => {
+            mainImage.src = e.target.getAttribute('data-large-image');
+        });
+    });
+
+    // Initially update price when page loads
+    updatePrice(quantity);
+});
