277f7aec3c3d9450ff32281ed09aaabec01e8b97
Broccoli <22130191@st.hcmuaf.edu.vn>
2024-11-24 22:26:39 +0700

    upload address card and change password card

--- profile/script.js
+++ profile/script.js
@@ -63,3 +63,49 @@ $(document).ready(function () {
     }
 
 });
+
+// Hàm sửa địa chỉ
+$(document).ready(function() {
+    // Khi nhấn nút Lưu địa chỉ
+    $('#addressSubmitButton').click(function() {
+        var fullName = $('#fullName').val();
+        var phoneNumber = $('#phoneNumber').val();
+        var address = $('#address').val();
+
+        // Kiểm tra nếu tất cả các trường đều có giá trị
+        if (fullName && phoneNumber && address) {
+            // Hiển thị thông tin đã nhập vào thẻ hiển thị
+            $('#displayFullName').text(fullName);
+            $('#displayPhoneNumber').text(phoneNumber);
+            $('#displayAddress').text(address);
+
+            // Hiển thị thẻ thông tin địa chỉ
+            $('#addressDisplay').show();
+
+            // Ẩn form nhập địa chỉ
+            $('#addressForm').hide();
+        } else {
+            // Hiển thị thông báo lỗi nếu một trong các trường bị thiếu
+            if (!fullName) $('#fullNameError').removeClass('d-none');
+            if (!phoneNumber) $('#phoneNumberError').removeClass('d-none');
+            if (!address) $('#addressError').removeClass('d-none');
+        }
+    });
+
+    // Tắt lỗi khi người dùng bắt đầu nhập lại
+    $('#fullName, #phoneNumber, #address').on('input', function() {
+        $(this).siblings('small').addClass('d-none');
+    });
+
+    // Khi nhấn nút "Sửa địa chỉ"
+    $('#editAddressButton').click(function() {
+        // Hiển thị lại form nhập địa chỉ
+        $('#addressForm').show();
+        $('#addressDisplay').hide();
+
+        // Điền lại các giá trị vào form để chỉnh sửa
+        $('#fullName').val($('#displayFullName').text());
+        $('#phoneNumber').val($('#displayPhoneNumber').text());
+        $('#address').val($('#displayAddress').text());
+    });
+});
