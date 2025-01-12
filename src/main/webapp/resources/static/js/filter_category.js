3549d060c379b44b75ec6901279e0ac03be19300
minhhien <22130080@st.hcmuaf.edu.vn>
2025-01-12 08:54:14 +0700

    giao diện servlet của filter

--- src/main/webapp/resources/static/js/filter_category.js
+++ src/main/webapp/resources/static/js/filter_category.js
@@ -9,19 +9,28 @@ $(document).ready(function() {
 		$(this).addClass('drop-brand-item__active');
 		var selectedText = $(this).text();
 		$('#dropdown-text-brand').text(selectedText);
+		var id = $(this).data('id');
+		$('input[name="brandId"]').val(id);
 	});
 	//Sự kiện chọn loại filter
 	$('.filter-item').on('click', function() {
-		$(this).addClass('filter-item__active');
-	});
-	//xóa tất cả filter đã chọn
-	$('#filter-remove-button').on('click', function() {
-		$('.filter-item').removeClass('filter-item__active');
-		$('#dropdown-text-brand').text('');
-		$('.dropdown-brand-item').removeClass('drop-brand-item__active');
-	});
-	$('#filter-submit-button').on('click', function() {
-		$('.collapse-content').hide(500);
+		var target = $(this).data('target');
+		var id = $(this).data('id');
+		if (target == 'category') {
+			$('input[name="categoryId"]').val(id);
+			$('.filter-category').removeClass('filter-item__active');
+		}
+		else if (target == "attributes") {
+			let purposes = $('input[name="attributesId"]').val();
+			    purposes = purposes ? purposes.split(','): [];
+			if (purposes.includes(`${id}`)) {
+				purposes = purposes.filter(item => item !== `${id}`);
+			} else {
+				purposes.push(id);
+			}
+			$('input[name="attributesId"]').val([...new Set(purposes)].join(','));
+		}
+		$(this).toggleClass('filter-item__active');
 
 	});
 });
